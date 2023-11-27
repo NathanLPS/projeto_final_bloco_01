fda6afebbeb0e2fddf44987a98d82264bda97fd7
nathan <nathan_lopes147@hotmail.com>
2023-11-27 15:16:06 -0300

    Model_repository

---
+++ OneDrive/Área de Trabalho/Ecomerce/Projeto_final_bloco_01/src/ecommerce/model/Produto.java
@@ -0,0 +1,64 @@
+package ecommerce.model;
+
+public class Produto {
+
+	private int tipo;
+	private int id;
+	private String nome;
+	private float preco;
+
+	public Produto(int tipo, int id, String nome, float preco) {
+		this.tipo = tipo;
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public void visualizar() {
+		String tipo = "";
+
+		switch (this.tipo) {
+		case 1 -> tipo = "Ferramenta";
+		case 2 -> tipo = "Material";
+		}
+
+		System.out.println(" Dados do Produto ");
+		System.out.println("                  ");
+		System.out.println(" Id Produto: " + this.id);
+		System.out.println(" Tipo do Produto: " + tipo);
+		System.out.println(" Preço do Produto (R$) " + this.preco);
+	}
+
+}
