413da60460719c0b8e474be09f7c92966f88cf0b
nathan <nathan_lopes147@hotmail.com>
2023-11-27 14:22:39 -0300

    Menu

--- OneDrive/Área de Trabalho/Ecomerce/Projeto_final_bloco_01/src/ecommerce/Menu.java
+++ OneDrive/Área de Trabalho/Ecomerce/Projeto_final_bloco_01/src/ecommerce/Menu.java
@@ -1,10 +1,68 @@
 package ecommerce;
 
+import java.util.Scanner;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner leia = new Scanner(System.in);
+
+		int opcao;
+
+		while (true) {
+
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                   Tonhão Materiais                  ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Cadastrar produto                    ");
+			System.out.println("            2 - Listar todas os Produtos             ");
+			System.out.println("            3 - Buscar produto por ID                ");
+			System.out.println("            4 - Atualizar Produto                    ");
+			System.out.println("            5 - Deletar Produto                      ");
+			System.out.println("            6 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+
+			opcao = leia.nextInt();
+
+			if (opcao == 6) {
+				System.out.println("\nTonhão Materiais- Sua contrução é aqui!");
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+			case 1:
+				System.out.println("Cadastrar Produto\n\n");
+
+				break;
+			case 2:
+				System.out.println("Listar todos os Produtos\n\n");
+
+				break;
+			case 3:
+				System.out.println("Buscar Produto por ID\n\n");
+
+				break;
+			case 4:
+				System.out.println("Atualizar Produto\n\n");
+
+				break;
+			case 5:
+				System.out.println("Deletar Produto\n\n");
+
+				break;
+
+			default:
+				System.out.println("\nOpção Inválida!\n");
+				break;
+			}
+		}
 	}
 
 }
